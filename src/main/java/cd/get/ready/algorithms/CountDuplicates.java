c24fe4299a081d56008a8ef069ae53ad6fab8ef6
davidupnext <dadeleke31@gmail.com>
2022-07-26 10:45:34 -0400

    done

--- src/main/java/cd/get/ready/algorithms/CountDuplicates.java
+++ src/main/java/cd/get/ready/algorithms/CountDuplicates.java
@@ -1,13 +1,33 @@
 package cd.get.ready.algorithms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountDuplicates {
     /**
      * Example of overloading a function that accepts a Array String
      * @param input
      * @return
      */
-    public Integer countDuplicates(String[] input){
-        return null;
+    public Integer countDuplicates(String[] input) {
+        Map<String, Integer> duplicates = new HashMap<String, Integer>();
+        for (String i : input) {
+            if (duplicates.containsKey(i)) {
+                duplicates.put(i, duplicates.get(i) + 1);
+            } else
+                duplicates.put(i, 1);
+        }
+        return duplicateLetters(duplicates);
+    }
+    private Integer duplicateLetters(Map <String, Integer> map) {
+        int result = 0;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                result++;
+            }
+        }
+
+        return result;
     }
 
     /**
@@ -15,7 +35,24 @@ public class CountDuplicates {
      * @param input
      * @return
      */
-    public Integer countDuplicates(Integer[] input){
-        return null;
+    public Integer countDuplicates(Integer[] input) {
+        Map<Integer, Integer> duplicates = new HashMap<Integer, Integer>();
+        for (Integer i : input) {
+            if (duplicates.containsKey(i)) {
+                duplicates.put(i, duplicates.get(i) + 1);
+            } else
+                duplicates.put(i, 1);
+        }
+        return duplicateNumbers(duplicates);
+    }
+    private Integer duplicateNumbers(Map <Integer, Integer> map){
+        int result = 0;
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if (entry.getValue() > 1){
+                result++;
+            }
+        }
+
+        return result;
     }
 }
